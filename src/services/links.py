f0cc7c0dc823bd2bdb0244cf98ea9f204a382b0e
alelsandr <sanya10081999@gmail.com>
2025-08-15 21:14:36 +0700

    services

---
+++ src/services/links.py
@@ -0,0 +1,9 @@
+import secrets
+
+from config import settings
+
+
+def create_code() -> str:
+    """Create random string of length {settings.link.length} """
+    code = "".join(secrets.choice(settings.link.chars) for _ in range(settings.link.length))
+    return code
