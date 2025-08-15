3c67db4829592324db7f20c93b8a0bfd0ee18771
alelsandr <sanya10081999@gmail.com>
2025-08-15 21:13:30 +0700

    models

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,13 +1,12 @@
-from sqlalchemy import MetaData
+
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 
-from config import settings
+
 from core.utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
-    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     @declared_attr.directive
     def __tablename__(cls):
